7dcb68373a82a8f87b377b653bd13cfb5a8b16b5
learn2410 <learn2410@mail.ru>
2022-07-10 11:43:30 +0500

    clarification of required fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,10 +2,10 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField('Название',max_length=200)
-    title_en = models.CharField('Название на английском',max_length=200,default='')
-    title_jp = models.CharField('Название на японском',max_length=200,default='')
+    title_en = models.CharField('Название на английском',max_length=200,default='',blank=True)
+    title_jp = models.CharField('Название на японском',max_length=200,default='',blank=True)
     image = models.ImageField('Картинка',null=True,blank=True)
-    description = models.TextField('Описание',default='')
+    description = models.TextField('Описание',default='',blank=True)
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
@@ -23,15 +23,15 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon,on_delete=models.CASCADE,verbose_name='Покемон')
-    lon = models.FloatField('Долгота', null=True)
-    lat = models.FloatField('Широта', null=True)
+    lon = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
     appeared_at = models.DateTimeField('Появится в')
     disappeared_at = models.DateTimeField('Исчезнет в')
-    level = models.IntegerField('Уровень',default=0)
-    health = models.IntegerField('Здоровье',default=0)
-    strength = models.IntegerField('Атака',default=0)
-    defencr = models.IntegerField('Защита',default=0)
-    stamins = models.IntegerField('Выносливость',default=0)
+    level = models.IntegerField('Уровень',default=0,blank=True,null=True)
+    health = models.IntegerField('Здоровье',default=0,blank=True,null=True)
+    strength = models.IntegerField('Атака',default=0,blank=True,null=True)
+    defencr = models.IntegerField('Защита',default=0,blank=True,null=True)
+    stamins = models.IntegerField('Выносливость',default=0,blank=True,null=True)
 
     class Meta:
         verbose_name = 'Покемон'
