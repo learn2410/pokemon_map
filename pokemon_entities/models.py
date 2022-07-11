99366fd7f34b5c5a4a6cce851ae960709c5657e5
learn2410 <learn2410@mail.ru>
2022-07-11 23:49:17 +0500

    fix naming

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,7 +11,7 @@ class Pokemon(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name="next_evolution",
+        related_name="next_evolutions",
         verbose_name="из кого эволюционировал")
 
     class Meta:
